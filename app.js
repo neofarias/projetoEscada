3fe2b6c2c773577866e9b9336b6057071ad93b09
neofarias <marciofsctt@gmail.com>
2025-04-19 18:16:00 -0300

    Criado o EJS

--- app.js
+++ app.js
@@ -3,5 +3,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//Pasta principal do projeto
+app.use(express.static('src'));
+
+//Diretório arquivos HTML
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+//Página inicial
+app.get('', (req, res) => {
+    res.render('index', { text: 'Hey' })
+})
+
 // Listen on Port 3000
 app.listen(port, () => console.info(`App listening on port ${port}`))
