bbf0aa55a85c4af87c59aec69c19592d0f7e389c
neofarias <marciofsctt@gmail.com>
2025-04-19 18:25:33 -0300

    Adicionado app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+// Imports
+const express = require('express')
+const app = express()
+const port = 3000
+
+//Pasta principal do projeto
+app.use(express.static('src'));
+
+//Diretório arquivos HTML
+app.set('views', 'projetoEscada/src/views');
+app.set('view engine', 'ejs');
+
+//Página inicial
+app.get('', (req, res) => {
+    res.render('index', { text: 'Hey' })
+})
+
+// Listen on Port 3000 no navegador
+app.listen(port, () => console.info(`App listening on port ${port}`))
